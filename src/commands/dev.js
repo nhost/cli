322eb3074d6c210a6a6f551b6cad2cef7f93c1be
Nuno Pato <nunopato@gmail.com>
2020-04-01 15:23:08 +0000

    Use prettier

--- src/commands/dev.js
+++ src/commands/dev.js
@@ -1,31 +1,65 @@
-const { Command, flags } = require('@oclif/command')
-const fs = require ('fs');
-const { execSync } = require('child_process');
-const yaml = require('js-yaml');
-const nunjucks = require('nunjucks');
+const { Command, flags } = require("@oclif/command");
+const fs = require("fs");
+const { execSync } = require("child_process");
+const yaml = require("js-yaml");
+const nunjucks = require("nunjucks");
+const crypto = require("crypto");
 
 class DevCommand extends Command {
   async run() {
-    if (!fs.existsSync('./docker-compose.example') || !fs.existsSync('./config.yaml')) {
-      return this.log('Please run `nhost-cli init` before starting a development environment.');
+    if (
+      !fs.existsSync("./docker-compose.example") ||
+      !fs.existsSync("./config.yaml")
+    ) {
+      return this.log(
+        "Please run `nhost-cli init` before starting a development environment."
+      );
     }
 
-    const nhostConfig = yaml.safeLoad(fs.readFileSync('config.yaml', { encoding: 'utf8' }));
-    const dockerComposeTemplate = fs.readFileSync('docker-compose.example', { encoding: 'utf8' });
+    const nhostConfig = yaml.safeLoad(
+      fs.readFileSync("config.yaml", { encoding: "utf8" })
+    );
+    const dockerComposeTemplate = fs.readFileSync("docker-compose.example", {
+      encoding: "utf8"
+    });
 
-    fs.writeFileSync('docker-compose.yaml', nunjucks.renderString(dockerComposeTemplate, nhostConfig));
-    execSync('docker-compose up -d');
-    this.log('services are launching...');
+    const jwtSecret = crypto
+      .randomBytes(128)
+      .toString("hex")
+      .slice(0, 128);
+
+    nhostConfig.graphql_jwt_key = jwtSecret;
+    fs.writeFileSync(
+      "docker-compose.yaml",
+      nunjucks.renderString(dockerComposeTemplate, nhostConfig)
+    );
+
+    const dockerFirstRun = !fs.existsSync("./db_data");
+
+    // validate compose file
+    execSync("docker-compose -f ./docker-compose.yaml config");
+    execSync("docker-compose up -d > /dev/null 2>&1");
+
+    this.log(
+      `development environment is launching...console will be launched at http://localhost:${nhostConfig.graphql_server_port}`
+    );
     
+    if (dockerFirstRun) {
+      this.log(
+        "This seems to be the first time running nhost dev in this project so it might take longer to start..."
+      );
+    }
+
     // check whether the graphql-engine endpoint is up & running
     let reachable = false;
     while (!reachable) {
       try {
-        execSync(`hasura console --endpoint=http://localhost:${nhostConfig.graphql_server_port} --admin-secret=${nhostConfig.graphql_admin_secret}`);
+        execSync(
+          `hasura console --endpoint=http://localhost:${nhostConfig.graphql_server_port} --admin-secret=${nhostConfig.graphql_admin_secret} > /dev/null 2>&1`
+        );
       } catch (Error) {
         continue;
       }
-      reachable = true;
     }
   }
 }
@@ -33,12 +67,12 @@ class DevCommand extends Command {
 DevCommand.description = `Describe the command here
 ...
 Extra documentation goes here
-`
+`;
 
 DevCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
-
-module.exports = DevCommand
+  name: flags.string({ char: "n", description: "name to print" })
+};
 
 nunjucks.configure({ autoescape: true });
+
+module.exports = DevCommand;
--- src/commands/init.js
+++ src/commands/init.js
@@ -1,7 +1,7 @@
-const { Command, flags } = require('@oclif/command')
-const fs = require('fs');
-const { exec } = require('child_process');
-const crypto = require('crypto');
+const { Command, flags } = require("@oclif/command");
+const fs = require("fs");
+const { exec } = require("child_process");
+const crypto = require("crypto");
 
 const composeContent = `version: '3.6'
 services:
@@ -28,8 +28,9 @@ services:
       HASURA_GRAPHQL_ENABLE_CONSOLE: 'false'
       HASURA_GRAPHQL_ENABLED_LOG_TYPES: startup, http-log, webhook-log, websocket-log, query-log
       HASURA_GRAPHQL_ADMIN_SECRET: {{ graphql_admin_secret }}
-      HASURA_GRAPHQL_JWT_SECRET: '{"type":"HS256", "key": "50a06d3b2b5114a164b66dec15db14214c585426bc67f7329679b53a2758029a9ce7894c3f464e8adac860ecaa50fa1ae1bb079c393f9360706adc047170f12d"}'
-      SECRET_WEBHOOK_HEADER: f3580e0add74a540c7261f6f4ee22dc8836837b29acd9a3618ebbeea91fd7ee5
+      HASURA_GRAPHQL_JWT_SECRET: '{"type":"HS256", "key": "{{ graphql_jwt_key }}"}'
+      HASURA_GRAPHQL_MIGRATIONS_SERVER_TIMEOUT: 5
+      HASURA_GRAPHQL_NO_OF_RETRIES: 5
     command:
       - graphql-engine
       - serve
@@ -46,16 +47,9 @@ services:
       USER_REGISTRATION_AUTO_ACTIVE: 'true'
       HASURA_GRAPHQL_ENDPOINT: http://nhost-graphql-engine:{{ graphql_server_port }}/v1/graphql
       HASURA_GRAPHQL_ADMIN_SECRET: {{ graphql_admin_secret }}
-      HASURA_GRAPHQL_JWT_SECRET: '{"type":"HS256", "key": "51a06d3b2b5114a164b66dec15db14214c585426bc67f7329679b53a2758029a9ce7894c3f464e8adac860ecaa50fa1ae1bb079c393f9360706adc047170f12d"}'
+      HASURA_GRAPHQL_JWT_SECRET: '{"type":"HS256", "key": "{{ graphql_jwt_key }}"}'
       AUTH_ACTIVE: 'true'
       AUTH_LOCAL_ACTIVE: 'true'
-      AUTH_GITHUB_ACTIVE: 'true'
-      AUTH_GITHUB_CLIENT_ID: 'aee57448edd447dc3379'
-      AUTH_GITHUB_CLIENT_SECRET: 'cd406c0a99ee6883a64264d72bcaea454cae6d83'
-      AUTH_GITHUB_CALLBACK_URL: 'http://localhost:9000/auth/github/callback'
-      AUTH_GOOGLE_ACTIVE: 'false'
-      AUTH_FACEBOOK_ACTIVE: 'false'
-      PROVIDERS_SUCCESS_REDIRECT: 'http://localhost:3000'
       REFRESH_TOKEN_EXPIRES: 43200
       JWT_TOKEN_EXPIRES: 15
     ports:
@@ -76,23 +70,23 @@ postgres_version: 12.0
 postgres_port: 5432
 postgres_user: postgres
 postgres_password: postgres
-`
+`;
     return configData;
   }
 
   async run() {
-    const {flags} = this.parse(InitCommand)
-    const directory = flags.directory || 'nhost_project'
+    const { flags } = this.parse(InitCommand);
+    const directory = flags.directory || "nhost_project";
 
     // make sure the provided directory does not exist
     if (!fs.existsSync(directory)) {
       fs.mkdirSync(directory);
     } else {
-      return this.log('Directory already exists');
+      return this.log("Directory already exists");
     }
 
-    // create docker-compose.yaml 
-    const dockerComposeFile = `./${directory}/docker-compose.example`; 
+    // create docker-compose.yaml
+    const dockerComposeFile = `./${directory}/docker-compose.example`;
     fs.writeFileSync(dockerComposeFile, composeContent);
     this.log(`${dockerComposeFile} created`);
 
@@ -103,15 +97,22 @@ postgres_password: postgres
 
     const configYamlFile = `./${directory}/config.yaml`;
     fs.writeFileSync(
-      configYamlFile, 
-      this.getConfigData(crypto.randomBytes(32).toString('hex').slice(0, 32))
+      configYamlFile,
+      this.getConfigData(
+        crypto
+          .randomBytes(32)
+          .toString("hex")
+          .slice(0, 32)
+      )
     );
     this.log(`${configYamlFile} created`);
 
     // finally check if hasura's CLI is installed
-    exec('command -v hasura', (error) => {
+    exec("command -v hasura", error => {
       if (error) {
-        this.log('The hasura CLI is a dependency. Please check out the installation instructions here https://hasura.io/docs/1.0/graphql/manual/hasura-cli/install-hasura-cli.html');
+        this.log(
+          "The hasura CLI is a dependency. Please check out the installation instructions here https://hasura.io/docs/1.0/graphql/manual/hasura-cli/install-hasura-cli.html"
+        );
       }
     });
   }
@@ -120,10 +121,13 @@ postgres_password: postgres
 InitCommand.description = `Describe the command here
 ...
 Extra documentation goes here
-`
+`;
 
 InitCommand.flags = {
-  directory: flags.string({char: 'd', description: 'directory where to create the files'}),
-}
+  directory: flags.string({
+    char: "d",
+    description: "directory where to create the files"
+  })
+};
 
-module.exports = InitCommand
+module.exports = InitCommand;
