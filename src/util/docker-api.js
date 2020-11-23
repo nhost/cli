bf79da42f6a62c7fcceb5c38634dab7894e26eee
elitan <johan@eliasson.me>
2020-11-23 08:36:00 +0100

    text update

--- src/util/docker-api.js
+++ src/util/docker-api.js
@@ -5,7 +5,7 @@ COPY package*.json yarn.lock ./
 RUN yarn install
 RUN yarn add express node-dir @babel/core @babel/cli @babel/preset-env @babel/polyfill @babel/plugin-transform-runtime @babel/node nodemon
 
-# Unable to use COPY since we don't know if the API folder exists.
+# Unable to use COPY since we don't know if the api/ folder exists.
 # So we need to juggle the folders a bit
 RUN mkdir /usr/src/app/src/api
 ADD . /tmp/app
