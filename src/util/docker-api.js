b723045715f9361814d20ecb80eab0d9fc42ecac
elitan <johan@eliasson.me>
2020-12-14 20:48:54 +0100

    kill hasura console manually

--- src/util/docker-api.js
+++ src/util/docker-api.js
@@ -1,12 +1,12 @@
 const dockerApiTemplate = `
-FROM nhost/nodeapi:v0.1.4
+FROM nhost/nodeapi:v0.2.0
 WORKDIR /usr/src/app
 
 COPY api ./api
 
 RUN ./install.sh
 
-CMD ["./node_modules/.bin/nodemon", "--exec", "./node_modules/.bin/babel-node", "index.js"]
+ENTRYPOINT ["./entrypoint.sh"]
 `;
 function getDockerApiTemplate() {
   return dockerApiTemplate.trim();
