b6a07842ea2fcd793b431c6302cf002bbef8c0ac
elitan <johan@eliasson.me>
2020-12-17 06:14:06 +0100

    using new entrypoint-dev.sh file locally

--- src/util/docker-api.js
+++ src/util/docker-api.js
@@ -1,12 +1,12 @@
 const dockerApiTemplate = `
-FROM nhost/nodeapi:v0.2.0
+FROM nhost/nodeapi:v0.2.1
 WORKDIR /usr/src/app
 
 COPY api ./api
 
 RUN ./install.sh
 
-ENTRYPOINT ["./entrypoint.sh"]
+ENTRYPOINT ["./entrypoint-dev.sh"]
 `;
 function getDockerApiTemplate() {
   return dockerApiTemplate.trim();
