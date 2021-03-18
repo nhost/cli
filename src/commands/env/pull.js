87b5892eb5946961335db41e67e5b3f7cfe7db5c
elitan <johan@eliasson.me>
2021-03-18 07:37:39 +0100

    nhost env:pull for dev env vars working

--- src/commands/env/pull.js
+++ src/commands/env/pull.js
@@ -1,14 +1,16 @@
 const { Command } = require("@oclif/command");
 const chalk = require("chalk");
+const yaml = require("js-yaml");
 const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const fs = require("fs");
+
+const { validateAuth } = require("../../util/login");
 
 class DownCommand extends Command {
   async run() {
-    this.log(`\n${chalk.white("bbb...")}`);
-
+    this.log(`Overwriting existing .env.development file`);
     const projectConfig = yaml.safeLoad(
-      await readFile(`.nhost/nhost.yaml`, { encoding: "utf8" })
+      fs.readFileSync(`.nhost/nhost.yaml`, { encoding: "utf8" })
     );
     const projectId = projectConfig.project_id;
 
@@ -21,8 +23,69 @@ class DownCommand extends Command {
     ];
 
     const project = projects.find((project) => project.id === projectId);
+    this.log(
+      `Downloading development environment variables for project: ${project.name}`
+    );
 
     const envFile = `.env.development`;
+
+    var envFileContent = fs.readFileSync(envFile, { encoding: "utf8" });
+
+    const existingEnvVars = envFileContent
+      .split("\n")
+      .filter((row) => {
+        return row;
+      })
+      .map((row) => {
+        const [name, value] = row.split("=");
+        return {
+          name,
+          value,
+        };
+      });
+
+    const updatedProjectEnvVarIndexs = [];
+
+    // update env vars already in .env.development
+    const envVars = existingEnvVars.map((existingEnvVar) => {
+      const i = project.project_env_vars.findIndex(
+        (pEnvVar) => pEnvVar.name == existingEnvVar.name
+      );
+      if (i === -1) {
+        return existingEnvVar;
+      }
+
+      const tmpEnvVar = project.project_env_vars[i];
+      updatedProjectEnvVarIndexs.push(i);
+
+      const res = {
+        name: existingEnvVar.name,
+        value: tmpEnvVar.dev_value,
+      };
+      return res;
+    });
+
+    // add env vars not already in .env.development
+    project.project_env_vars
+      .filter((_, i) => {
+        // filter if the env var was alrady updated
+        return !updatedProjectEnvVarIndexs.includes(i);
+      })
+      .forEach((envVar) => {
+        // add new env var
+        envVars.push({
+          name: envVar.name,
+          value: envVar.dev_value,
+        });
+      });
+
+    fs.writeFileSync(
+      envFile,
+      envVars.map((envVar) => `${envVar.name}=${envVar.value}`).join("\n"),
+      { flag: "w" }
+    );
+
+    this.log(`${chalk.white("✅  .env.development file updated")}`);
   }
 }
 
