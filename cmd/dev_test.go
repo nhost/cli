7239f78627568e425c3b0fad142a10b940510dd1
Mrinal Wahal <mrinal_w@rocketmail.com>
2021-11-30 14:36:28 +0530

    tests: changed temp dir ownership

--- cmd/dev_test.go
+++ cmd/dev_test.go
@@ -123,8 +123,17 @@ func Test_Pipeline(t *testing.T) {
 	}
 
 	//	Take ownership of minio location before removing temp dir
-	if err := syscall.Unlink(filepath.Join(nhost.DOT_NHOST, "minio", "data", ".minio.sys", "buckets", ".tracker.bin")); err != nil {
-		t.Errorf("Failed to unlunk minio location: %v", err)
+	file := filepath.Join(nhost.DOT_NHOST, "minio", "data", ".minio.sys", "buckets", ".tracker.bin")
+	if err := os.Chmod(file, 0777); err != nil {
+		t.Errorf("Failed to take ownership of %s: %v", file, err)
+	}
+
+	if err := os.Chown(file, os.Geteuid(), os.Getgid()); err != nil {
+		t.Errorf("Failed to take ownership of %s: %v", file, err)
+	}
+
+	if err := syscall.Unlink(file); err != nil {
+		t.Errorf("Failed to unlink minio location: %v", err)
 	}
 
 	/*
