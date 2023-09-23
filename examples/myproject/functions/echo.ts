44603585e035bff882aae4e4b84161546e65e757
David Barroso <dbarrosop@dravetech.com>
2023-09-23 08:17:30 +0200

    chore: example: added architecture

--- examples/myproject/functions/echo.ts
+++ examples/myproject/functions/echo.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express'
 import process from 'process'
 
+{} satisfies {}
+
 export default (req: Request, res: Response) => {
     res.status(200).json(
         {
             headers: req.headers,
             query: req.query,
             node: process.version,
+            arch: process.arch,
         },
     )
 }
