11aa2c79f495b94d773c76d702ef8e02d3e9ffa9
onehassan <onehassan@users.noreply.github.com>
2024-09-27 15:09:39 +0000

    chore: bump nhost/dashboard to 1.30.0

--- cmd/dev/up.go
+++ cmd/dev/up.go
@@ -110,7 +110,7 @@ func CommandUp() *cli.Command { //nolint:funlen
 			&cli.StringFlag{ //nolint:exhaustruct
 				Name:    flagDashboardVersion,
 				Usage:   "Dashboard version to use",
-				Value:   "nhost/dashboard:1.27.0",
+				Value:   "nhost/dashboard:1.30.0",
 				EnvVars: []string{"NHOST_DASHBOARD_VERSION"},
 			},
 			&cli.StringFlag{ //nolint:exhaustruct
