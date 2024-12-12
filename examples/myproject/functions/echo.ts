96716342de0ec9f1b8063686c0c7311022eeb553
David Barroso <dbarrosop@dravetech.com>
2024-12-12 09:35:03 +0100

    sad

--- examples/myproject/functions/echo.ts
+++ examples/myproject/functions/echo.ts
@@ -3,13 +3,13 @@ import process from 'process'
 import zlib from 'zlib'
 
 export default (req: Request, res: Response) => {
-    const response = {
+    var response = {
         headers: req.headers,
         query: req.query,
         node: process.version,
         arch: process.arch,
         data: {
-            message: 'Hello, World!',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         },
     }
 
