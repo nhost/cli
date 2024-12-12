edbf413b4a6b877c0c2313373a633c200e79e08a
David Barroso <dbarrosop@dravetech.com>
2024-12-12 09:29:37 +0100

    asd

--- examples/myproject/functions/echo.ts
+++ examples/myproject/functions/echo.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from 'express'
 import process from 'process'
+import zlib from 'zlib'
 
 export default (req: Request, res: Response) => {
-    res.status(200).json(
-        {
-            headers: req.headers,
-            query: req.query,
-            node: process.version,
-            arch: process.arch,
+    const response = {
+        headers: req.headers,
+        query: req.query,
+        node: process.version,
+        arch: process.arch,
+        data: {
+            message: 'Hello, World!',
         },
-    )
+    }
+
+    // compress the response
+    res.setHeader('Content-Encoding', 'gzip')
+    res.setHeader('Content-Type', 'application/json')
+
+    const compressedResponse = zlib.gzipSync(JSON.stringify(response))
+
+    res.send(compressedResponse)
 }
