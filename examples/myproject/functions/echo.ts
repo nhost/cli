a8527c5625090ecede7c3f03e055d2d1becb85e5
David Barroso <dbarrosop@dravetech.com>
2025-04-08 22:21:12 +0200

    asd

--- examples/myproject/functions/echo.ts
+++ examples/myproject/functions/echo.ts
@@ -1,13 +1,27 @@
-import { Request, Response } from 'express'
+import { createGzip } from 'zlib'
+import { IncomingMessage, ServerResponse } from 'http'
 import process from 'process'
 
-export default (req: Request, res: Response) => {
-    res.status(200).json(
-        {
-            headers: req.headers,
-            query: req.query,
-            node: process.version,
-            arch: process.arch,
-        },
-    )
+export default (req: IncomingMessage, res: ServerResponse) => {
+    const responseData = {
+        headers: req.headers,
+        query: new URL(req.url || '', `http://${req.headers.host}`).searchParams,
+        method: req.method,
+        node: process.version,
+        arch: process.arch,
+    }
+
+    const jsonString = JSON.stringify(responseData)
+    const acceptsGzip = req.headers['accept-encoding']?.includes('gzip')
+
+    if (acceptsGzip) {
+        res.setHeader('Content-Encoding', 'gzip')
+        res.setHeader('Content-Type', 'application/json')
+        const gzip = createGzip()
+        gzip.pipe(res)
+        gzip.end(jsonString)
+    } else {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(jsonString)
+    }
 }
