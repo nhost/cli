a92aa0906a0acd65bb7206cca4ef473fb4094a8d
David BM <correodelnino@gmail.com>
2025-04-14 01:17:42 +0200

    fix: mock live app state for postgres version change

--- cmd/configserver/querier.go
+++ cmd/configserver/querier.go
@@ -2,6 +2,8 @@ package configserver
 
 import (
 	"context"
+	"runtime"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -9,5 +11,21 @@ import (
 type Querier struct{}
 
 func (q Querier) GetAppDesiredState(_ context.Context, _ uuid.UUID) (int32, error) {
-	return 5, nil //nolint:mnd
+	// Get the caller two levels up
+	pc := make([]uintptr, 10)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+
+	for {
+		frame, more := frames.Next()
+		if strings.Contains(frame.Function, "changeDatabaseVersionValidate") {
+			return 5, nil
+		}
+		if !more {
+			break
+		}
+	}
+	
+	// Default to appPaused for all other cases
+	return 6, nil //nolint:mnd
 }
