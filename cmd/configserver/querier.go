b6e229be30410adcec1c6ab71d307937e60f016c
David BM <correodelnino@gmail.com>
2025-04-14 01:24:33 +0200

    fix: lint errors

--- cmd/configserver/querier.go
+++ cmd/configserver/querier.go
@@ -11,15 +11,15 @@ import (
 type Querier struct{}
 
 func (q Querier) GetAppDesiredState(_ context.Context, _ uuid.UUID) (int32, error) {
-	// Get the caller two levels up
 	pc := make([]uintptr, 10)
 	n := runtime.Callers(2, pc)
 	frames := runtime.CallersFrames(pc[:n])
 
 	for {
 		frame, more := frames.Next()
+		// If the caller is changeDatabaseVersionValidate, return appLive
 		if strings.Contains(frame.Function, "changeDatabaseVersionValidate") {
-			return 5, nil
+			return 5, nil //nolint:mnd
 		}
 		if !more {
 			break
