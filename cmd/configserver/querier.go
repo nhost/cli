a0a0c6ae35398b939c47a4a6213a117454e8f9da
David BM <correodelnino@gmail.com>
2025-04-14 14:31:11 +0200

    chore: mock empty desired state

--- cmd/configserver/querier.go
+++ cmd/configserver/querier.go
@@ -2,8 +2,6 @@ package configserver
 
 import (
 	"context"
-	"runtime"
-	"strings"
 
 	"github.com/google/uuid"
 )
@@ -11,21 +9,5 @@ import (
 type Querier struct{}
 
 func (q Querier) GetAppDesiredState(_ context.Context, _ uuid.UUID) (int32, error) {
-	pc := make([]uintptr, 10)
-	n := runtime.Callers(2, pc)
-	frames := runtime.CallersFrames(pc[:n])
-
-	for {
-		frame, more := frames.Next()
-		// If the caller is changeDatabaseVersionValidate, return appLive
-		if strings.Contains(frame.Function, "changeDatabaseVersionValidate") {
-			return 5, nil //nolint:mnd
-		}
-		if !more {
-			break
-		}
-	}
-	
-	// Default to appPaused for all other cases
-	return 6, nil //nolint:mnd
+	return 0, nil //nolint:mnd
 }
